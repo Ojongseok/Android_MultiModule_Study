ef9ae4ccf8b028328e229aaf6cbfa1a50e865820
Ojongseok <whdtjr6889@naver.com>
2023-12-13 05:50:56 +0900

    Gradle Version Catalog  설정

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.kotlin.stdlib)
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
